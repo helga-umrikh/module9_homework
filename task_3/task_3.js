b7473a442b8763501b5a0139eeb3b36101d10d31
helga-umrikh <zetaumr@gmail.com>
2023-02-24 16:44:40 +0300

    task.js changed all js algorythms, task completed

--- task_3/task_3.js
+++ task_3/task_3.js
@@ -1,26 +1,80 @@
-//Напишите код приложения, интерфейс которого представляет собой input и кнопку. В input можно ввести любое число. При клике на кнопку происходит следующее:
-// Если число не попадает в диапазон от 1 до 10 — выводить ниже текст «число вне диапазона от 1 до 10».
-// Если число попадает в диапазон от 1 до 10 — сделать запрос c помощью XHR по URLhttps://picsum.photos/v2/list?limit=10, где get-параметр limit — это введённое число.
-// Пример. Если пользователь ввёл 5, то запрос будет вида:https://picsum.photos/v2/list?limit=5.
-// После получения данных вывести ниже картинки на экран.
+/**
+ * Функция-обертка над XMLHttpRequest, осуществляющая запрос
+ * url - урл, по которому будет осуществляться запрос
+ * callback - функция, которая вызовется при успешном выполнении
+ * и первым параметром получит объект-результат запроса
+ */
+function useRequest(url, callback) {
+    if (validateInput()) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
 
-const btn = document.querySelector("#button");
-
-const checkNumberRange = (num) => {
-    if (num > 10 || num < 1) {
-        document.write("число вне диапазона от 1 до 10");
-    } else {
-        let xhr = new XMLHttpRequest();
-        xhr.onload = function () {
-            console.log(`Статус: ${xhr.status}; Результат: ${xhr.response}`);
+    xhr.onload = function () {
+        if (xhr.status != 200) {
+        console.log("Статус ответа: ", xhr.status);
+        } else {
+        const result = JSON.parse(xhr.response);
+        if (callback) {
+            callback(result);
+        }
+        }
     };
 
     xhr.onerror = function () {
-        console.log("Ошибка запроса");
+        console.log("Ошибка! Статус ответа: ", xhr.status);
     };
-    xhr.open("get", "https://picsum.photos/v2/list?limit=10", true);
+
     xhr.send();
     }
-};
+}
 
-button.addEventListener("click", checkNumberRange);
+function validateInput() {
+    let validated = true;
+    if (input.value === "" || isNaN(+input.value)) {
+    validated = false;
+    console.log("not a number");
+    }
+
+    if (input.value > 10 || input.value < 1) {
+    validated = false;
+    //доделать вывод строки на страницу
+    resultNode.innerHTML = "число вне диапазона от 1 до 10";
+    }
+    return validated;
+}
+
+// Ищем ноду для вставки результата запроса
+const resultNode = document.querySelector(".j-result");
+// Ищем кнопку, по нажатии на которую будет запрос
+const btnNode = document.querySelector(".j-btn-request");
+
+/**
+ * Функция обработки полученного результата
+ * apiData - объект с результатом запроса
+ */
+function displayResult(apiData) {
+    let cards = "";
+  // console.log('start cards', cards);
+
+    apiData.forEach((item) => {
+    const cardBlock = `
+        <div class="card">
+        <img
+            src="${item.download_url}"
+            class="card-image"
+            />
+            <p>${item.author}</p>
+        </div>
+        `;
+    cards = cards + cardBlock;
+    });
+
+  // console.log('end cards', cards);
+
+    resultNode.innerHTML = cards;
+}
+
+// Вешаем обработчик на кнопку для запроса
+btnNode.addEventListener("click", () => {
+    useRequest("https://picsum.photos/v2/list/?limit=5", displayResult);
+});
